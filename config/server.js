feb2647482d4008b8510467ab56087c403c5110a
Divine Dube <divine.dube@takealot.com>
2021-06-15 17:39:35 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36571168562bdf1d373f78acd0c55b7b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25000238-e014-4346-a94a-00c5a35da2f3'
+};

6e6f1c4c33fc9bd90b306968c35071eee706ed86
Divine Dube <divine.dube@takealot.com>
2021-06-23 14:49:00 +0400

    remove debug comments

--- api/blog/controllers/blog.js
+++ api/blog/controllers/blog.js
@@ -7,9 +7,16 @@
 
 module.exports = {
     async find(ctx) {
-        console.log("Hello here we gooo", ctx)
         const blogs = await strapi.services.blog.find(ctx.query)
-        console.log("Hello here we gooo again", blogs)
-        return blogs.map((blog) => ({ ...blog, author: { ...blog.author, profile_photo: { name: blog.author.profile_photo.name, thumbnailUrl: blog.author.profile_photo.formats.thumbnail.url }} }))
+        return blogs.map((blog) => ({
+             ...blog, 
+             author: { 
+                ...blog.author,
+                profile_photo: { 
+                    name: blog.author.profile_photo.name,
+                    thumbnailUrl: blog.author.profile_photo.formats.thumbnail.url
+                }
+            }
+        }))
     }
 };
